b7362ec79a78b8fd561005c1cd47d79594f22ab9
Silverscale <adrian.barcia@gmail.com>
2019-07-19 22:26:49 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+ 
+    public GameObject bodyChunk;
+
+    public int PlayerNumber    {
+        get; private set;
+    }
+
+    private Transform body;
+    private readonly string wallTag = "Obstacle";
+
+    public void Awake()
+    {
+        PlayerNumber = FindObjectsOfType<PlayerController>().Length;
+        Debug.Log("Created player " + PlayerNumber);
+        body = GameObject.Instantiate(bodyChunk, gameObject.transform, false).GetComponent<Transform>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(wallTag))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Setup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup : MonoBehaviour {
+
+    public PlayerController playerPrefab;
+    public Transform[] spawnPoint;
+    //public Collider headCollider;
+    //public Collider bodyCollider;
+
+    private Transform player;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        for (int i = 0; i < spawnPoint.Length; i++)
+        {
+            player = Object.Instantiate<GameObject>(playerPrefab.gameObject).GetComponent<Transform>();
+            //bodyCollider.enabled = false;
+            //headCollider.enabled = true;
+
+            player.position = spawnPoint[i].position;
+            player.rotation = spawnPoint[i].rotation;
+
+        }
+    }
+	
+}
