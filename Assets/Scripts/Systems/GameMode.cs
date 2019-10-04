dbea5fe38e843f8604d4b95210e56c126405981d
Silverscale <adrian.barcia@gmail.com>
2019-10-05 01:59:44 +0300

    GameMode Created
    But still not being used.

---
+++ Assets/Scripts/Systems/GameMode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Game Mode")]
+public class GameMode : ScriptableObject
+{
+    public static float BODY_EXPAND_DELAY;
+    public static int BODY_FRAMES_BEHIND;
+    public static float MAX_FORWARD_SPEED;
+    public static float TURNING_RATE;
+    public static float SNAKE_SCALE;
+    public static float DELAY_UNTIL_MAXSPEED;
+    public static bool DRUNK_DRAGONS;
+
+    public float delay;
+    public int framesBehind;
+    public float forward;
+    public float turning;
+    public float snakeScale;
+    public float maxSpeedDelay;
+    public bool drunkDragons;
+
+    public void Set() {
+        BODY_EXPAND_DELAY = delay;
+        BODY_FRAMES_BEHIND = framesBehind;
+        MAX_FORWARD_SPEED = forward;
+        TURNING_RATE = turning;
+        SNAKE_SCALE = snakeScale;
+        DELAY_UNTIL_MAXSPEED = maxSpeedDelay;
+        DRUNK_DRAGONS = drunkDragons;
+        Debug.Log("Game Mode changed to: " + this.name);
+    }
+}
---
+++ Assets/Scripts/UI/FillGameModeList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FillGameModeList : MonoBehaviour
+{
+    [SerializeField] private Button button = default;
+    [SerializeField] private Transform folder = default;
+
+    void Awake()
+    {
+        var modes = Resources.LoadAll<GameMode>("GameModes");
+        foreach (var mode in modes) {
+            Button newButton = Instantiate(button, folder);
+            newButton.onClick.AddListener(mode.Set);
+            newButton.GetComponentInChildren<TextMeshProUGUI>().text = mode.name;
+        }
+    }
+}
